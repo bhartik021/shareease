3e2d4cc50e3a37324529154691d6babc579b7a20
Bharti Kumari <bhartik021@gmail.com>
2023-10-17 16:28:44 +0530

    frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
+import React, { useRef, useState, useEffect } from 'react'; // Import React
+import { uploadFile } from './services/api';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [file, setFile] = useState(''); // Add a state variable to hold the file
+
+
+  const fileInputRef = useRef(); // Move the useRef inside the functional component
+
+  const onUploadClick = () => {
+    fileInputRef.current.click();
+  }
+
+  useEffect(() => {
+    const getImage = async () => {
+      if(file) {
+        const data = new FormData();
+        data.append("name", file.name);
+        data.append("file", file);
+
+        let response = await uploadFile(data);
+      }
+    }
+    getImage();
+  }, [file])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="wrapper"> 
+        <h1> ShareEase </h1>
+        <p> Upload and Share the Download Link </p>
+        <button onClick={() => onUploadClick()}>
+          Upload
+        </button>
+        <input type="file" 
+          ref={fileInputRef}
+          style={{display: 'none'}}
+          onChange={(e) => {
+            setFile(e.target.files[0])
+          }}
+        />
+      </div>
     </div>
   );
 }
